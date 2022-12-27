13472852c2928ba9d2f9d0a5f14d8d5ea0c7a42f
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-12-27 21:31:52 +0300

    first

---
+++ src/main/java/bank/util/PredicateUtil.java
@@ -0,0 +1,37 @@
+package bank.util;
+
+/**
+ * @author Furkan Özmen
+ */
+public class PredicateUtil {
+    public static <T> boolean isNull(T t) {
+        return t == null;
+    }
+
+    public static <T> boolean isNotNull(T t) {
+        return t != null;
+    }
+
+    public static <T> boolean isNull(T... t) {
+        for (T t1 : t) {
+            if (t1 == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T> boolean isNotNull(T... t) {
+        for (T t1 : t) {
+            if (t1 == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <T> T getOrDefault(T t, T defaultValue) {
+        return isNull(t) ? defaultValue : t;
+    }
+
+}
