70ab40d20c43eeacdb0e51bcb32aa4818cf8d42e
AErencihan <106157023+AErencihan@users.noreply.github.com>
2023-01-03 00:52:07 +0300

    second edit

--- src/main/java/bank/client/DashBoard.java
+++ src/main/java/bank/client/DashBoard.java
@@ -18,7 +18,6 @@ public class DashBoard extends JFrame {
     private final JButton ACCOUNTS = new JButton("Hesaplarım");
     private final JButton CREATE_ACCOUNT = new JButton("Hesap Aç");
     private final JButton DELETE_ACCOUNT = new JButton("Hesap Sil");
-    private final JButton ACCOUNT_DETAILS = new JButton("Hesap Detayları");
     private final JButton DEPOSIT = new JButton("Para Yatır");
     private final JButton WITHDRAW = new JButton("Para Çek");
     private final JButton TRANSFER = new JButton("Havale Yap");
@@ -29,7 +28,7 @@ public class DashBoard extends JFrame {
         super(title);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 8));
+        panel.setLayout(new GridLayout(2, 4));
 
         //panel.setLayout(new GridLayout(3, 8, 10, 10));
         panel.setBorder(BorderFactory.createLineBorder(new Color(144, 238, 144), 10));
@@ -45,10 +44,6 @@ public class DashBoard extends JFrame {
         panel.add(DELETE_ACCOUNT);
         DELETE_ACCOUNT.setForeground(new Color(0, 139, 139));
         DELETE_ACCOUNT.setFont(new Font("Arial", Font.BOLD, 20));
-
-        panel.add(ACCOUNT_DETAILS);
-        ACCOUNT_DETAILS.setForeground(new Color(0, 139, 139));
-        ACCOUNT_DETAILS.setFont(new Font("Arial", Font.BOLD, 20));
 
         panel.add(DEPOSIT);
         DEPOSIT.setForeground(new Color(0, 139, 139));
